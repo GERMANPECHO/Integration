937ba399d35cd0b97570faa8a98f21d424dffffc
GERPSZ <GERPSZ@HOTMAIL.COM>
2018-11-17 18:46:17 -0500

    TestsTravisEffNote

--- src/Test/TestEffNotes.java
+++ src/Test/TestEffNotes.java
@@ -16,12 +16,7 @@ public class TestEffNotes {
 	private Vector<String> notesLineal = new Vector<String>();
 	private Vector<String> aleatoireLineal = new Vector<String>();
 	private Vector<String> notesLinealAleatoire = new Vector<String>();
-	private Vector<String> linealAleatoireHeures = new Vector<String>();
-	private Vector<String> linealAleatoireNotes = new Vector<String>();
-	private Vector<String> linealAleatoireMaxDispersion = new Vector<String>();
-	private Vector<String> linealAleatoireMinDispersion = new Vector<String>();
-	private Vector<String> fortelinearHeures = new Vector<String>();
-	private Vector<String> fortelinearNotes = new Vector<String>();
+
 	
 	@Before 
     public void faireAvant() {
@@ -49,9 +44,6 @@ public class TestEffNotes {
 			valueHeures = valminHeuresAccumule + incrementLineal;
 			valueNotes = valminNotesAccumule + incrementNotes;
 			
-            valuesHeureNonLinear = Math.random()*101.00 + 0.00;
-			valuesNotesNonLinear = Math.log(valuesHeureNonLinear);
-			
 			
 			/*
 			 * valeurs limites entre le min et le max de la grille
@@ -65,19 +57,6 @@ public class TestEffNotes {
 			values_Heures = Math.random()*29.58 + 1.00;
 			values_Notes = Math.random()*101.00 + 1.00;	
 			
-			values_Heures_maxDispersion = Math.random()*144.00 + 0.00;
-			values_Notes_maxDispersion  = Math.random()*101.00 + 0.00;
-			
-			fortelinearHeures.add(String.valueOf(valuesHeureNonLinear));
-			fortelinearNotes.add(String.valueOf(valuesNotesNonLinear));
-			
-			
-			linealAleatoireMaxDispersion.add(String.valueOf(values_Heures_maxDispersion));
-			linealAleatoireMinDispersion.add(String.valueOf(values_Notes_maxDispersion ));
-			
-			
-			linealAleatoireHeures.add(String.valueOf(values_Heures));
-			linealAleatoireNotes.add(String.valueOf(values_Notes));
 			
 			lineal.add(String.valueOf(valueHeures));
 			aleatoireLineal.add(String.valueOf(valuesHeuresAleatoire));
@@ -117,30 +96,6 @@ public class TestEffNotes {
 		Correlation coefficient = new Correlation(notesLineal,lineal);
 		double coefficientCorrelation = coefficient.calculerCoeffCorrelation();
 		assertTrue(coefficientCorrelation != 0);
-	}
-	
-	@Test
-	public void testCalculeCorrelationLimitesMaxMins() {
-		Correlation coefficient = new Correlation(linealAleatoireNotes,linealAleatoireHeures);
-		double coefficientCorrelation = coefficient.calculerCoeffCorrelation();
-		System.out.println(coefficient.qualifierReponse(coefficientCorrelation));
-		assertTrue(coefficientCorrelation >= 0.00 && coefficientCorrelation <= 1.00);
-	}
-	
-	@Test
-	public void testCalculeCorrelationMaxDispersion() {
-		Correlation coefficient = new Correlation(linealAleatoireMaxDispersion,linealAleatoireMinDispersion);
-		double coefficientCorrelation = coefficient.calculerCoeffCorrelation();
-		assertTrue(coefficientCorrelation >= 0.00 && coefficientCorrelation <= 1.00);
-	}
-	
-	@Test
-	public void testFortLinearCoefficient() {
-		Correlation coefficient = new Correlation(fortelinearNotes,fortelinearHeures);
-		double coefficientCorrelation = coefficient.calculerCoeffCorrelation();
-		System.out.println(coefficient.qualifierReponse(coefficientCorrelation));
-		System.out.println("coeff "+ coefficientCorrelation);	
-		assertTrue(coefficientCorrelation >= 0.00 && coefficientCorrelation <= 1.00);
 	}
 	
 	
