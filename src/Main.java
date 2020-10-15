03b86a0aa66f5f78f3eda497da0e0bc22b545e1b
GERPSZ <GERPSZ@HOTMAIL.COM>
2020-10-15 00:33:57 -0400

    kl

--- src/Main.java
+++ src/Main.java
@@ -59,6 +59,17 @@ public class Main {
 
 	}
 	
+	public static double getDival(String values,Vector<String> vectorValues) {
+		
+		double somme = 0;
+		
+		for( int i=0; i< Integer.parseInt(values); i++ ) {
+			somme = somme + Double.parseDouble(vectorValues.get(i));
+		}
+		
+		return somme;
+	}
+	
 	
 	public static double getSomme(String values,Vector<String> vectorValues) {
 		
